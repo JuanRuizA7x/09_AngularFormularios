388a4c796d467510bb688224ccd960469e49ee0b
Juan Ruíz <juan.ruiz.22.03.00@gmail.com>
2020-06-26 22:07:25 -0500

    Inicio ReactiveComponent
    Se hicieron las validaciones en el formulario del ReactiveComponent

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -8,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  formReactive: FormGroup;
+
+  constructor( private formBuilder: FormBuilder ) {
+    this.createForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  createForm() {
+    this.formReactive = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+    });
+  }
+
+  save() {
+    if (this.formReactive.invalid) {
+      Object.values(this.formReactive.controls).forEach(control => {
+          control.markAllAsTouched();
+      });
+      return;
+    }
+    console.log(this.formReactive);
+    console.log(this.formReactive.value);
+  }
+
+  isValid(model: string) {
+    return this.formReactive.get(model).valid;
+  }
+
+  isInvalid(model: string) {
+    return this.formReactive.get(model).invalid && this.formReactive.get(model).touched;
   }
 
 }
